a43e676ded951848b34b14d03a2c70fd753d7414
Tanvir Ahmed Sojal <tanvirahmedsojal@gmail.com>
2019-02-06 21:24:24 +0600

    Create 1001 - Opposite Task.cpp

---
+++ 1001 - Opposite Task.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int T;
+    scanf("%d", &T);
+ 
+    while(T--)
+    {
+        int n;
+        scanf("%d", &n);
+ 
+        if (n<=10) printf("0 %d\n", n);
+        else printf("10 %d\n", (n-10));
+    }
+    return 0;
+}
