8e263b8208e1f0aeaf7916e2cc87a1f265ab3287
Tanvir Ahmed Sojal <tanvirahmedsojal@gmail.com>
2020-12-12 17:58:51 +0600

    Update 1059 - Air Ports.cpp

--- 1059 - Air Ports.cpp
+++ 1059 - Air Ports.cpp
@@ -1,155 +1,67 @@
 #include<bits/stdc++.h>
-#define CLR(a)              memset(a, 0, sizeof(a))
-#define SET(a, x)           memset(a, x, sizeof(a))
-#define SORT(v)             sort(v.begin(), v.end())
-#define REV(v)              reverse(v.begin(), v.end())
-#define FOR(i, n)           for (int i=0; i<n; i++)
-#define FOR1(i, n)          for (int i=1; i<=n; i++)
-#define ROF(i, n)           for (int i=n-1; i>=0; i--)
-#define ROF1(i, n)          for (int i=n; i>=1; i--)
-#define LOOP(i, x, n)       for (int i=x; i<=n; i++)
-#define READ(x)             freopen(x, "r", stdin);
-#define WRITE(x)            freopen(x, "w", stdout);
-#define fastIO()            ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define PI                  acos(-1.0)
-#define pb                  push_back
-#define mp                  make_pair
-#define fi                  first
-#define se                  second
- 
 using namespace std;
- 
-typedef long long              ll;
-typedef unsigned long long     ull;
-typedef vector<int>            vi;
-typedef pair<int, int>         pii;
-typedef pair<string, int>      psi;
-typedef pair<char, int>        pci;
- 
-const double eps = 1e-9;
- 
-int fx[] = {-1, 1, 0, 0, -1, -1, 1, 1};
-int fy[] = {0, 0, -1, 1, -1, 1, -1, 1};
- 
-int kx[] = {1, 1, 2, 2, -1, -1, -2, -2};
-int ky[] = {2, -2, 1, -1, 2, -2, 1, -1};
- 
-int TLx[] = {0, -1, -1, 0};    int TLy[] = {-1, -1, 0, 0};
-int TRx[] = {-1, -1, 0, 0};    int TRy[] = {0, 1, 1, 0};
-int BLx[] = {0, 1, 1, 0};      int BLy[] = {-1, -1, 0, 0};
-int BRx[] = {0, 1, 1, 0};      int BRy[] = {1, 1, 0, 0};
- 
-#define MAX 10010
+#define MX 100005
  
 struct edge{
     int u, v, w;
 };
- 
-bool cmp(edge a, edge b){
+bool cmp(edge &a, edge &b){
     return a.w < b.w;
 }
+vector< edge > G; // edge list
+int parent[MX];
  
-vector < edge > G;
-vector < int > graph[MAX];
-map < pii , bool > M;
-map < pii , int > C;
- 
-int parent[MAX];
-bool vis[MAX];
-int n, m, airport, cnt;
-ll cost, res;
+void reset(int n){
+    G.clear();
+    for (int i = 1; i <= n; i++)
+        parent[i] = i;
+}
  
 int Find(int u){
-    if (u==parent[u]) return u;
+    if (u == parent[u]) return u;
     return parent[u] = Find(parent[u]);
 }
  
-void reset(){
-    FOR(i, MAX){
-        graph[i].clear();
-        vis[i] = 0;
-        parent[i] = i;
-    }
-    G.clear();
-    cnt = 0;
-    res = 0;
-    M.clear();
-    C.clear();
-}
- 
-void MST(){
-    cost = 0;
+int mst(){
     sort(G.begin(), G.end(), cmp);
-    FOR(i, m){
-        int u = G[i].u;
-        int v = G[i].v;
-        int w = G[i].w;
-        int p = Find(u);
-        int q = Find(v);
-        if (p!=q){
-            pii x = mp(u, v);
-            pii y = mp(v, u);
-            M[x] = 1; M[y] = 1;
- 
+    int cost = 0;
+    for (int i = 0; i < G.size(); i++){
+        int p = Find(G[i].u);
+        int q = Find(G[i].v);
+        if (p != q){
             parent[q] = p;
+            cost += G[i].w;
         }
     }
+    return cost;
 }
- 
-void dfs(int n){
-    vis[n] = 1;
-    int sz = graph[n].size();
-    FOR(i, sz){
-        int v = graph[n][i];
-        pii x = mp(n, v);
-        if (!vis[v]){
-            int cst = C[x];
-            if (M[x]){
-                if (cst >= airport){
-                    ++cnt;
-                    res += airport;
-                }
-                else res += cst;
-                dfs(v);
-            }
-        }
-    }
-}
- 
- 
  
 int main()
 {
-    //READ("input.txt");
-    //WRITE("output.txt");
+//    freopen("in.txt", "r", stdin);
     int T; scanf("%d", &T);
-    FOR1(cs, T){
-        reset();
+    for (int cs = 1; cs <= T; cs++){
+        int n, m, airport;
         scanf("%d%d%d", &n, &m, &airport);
-        FOR(i, m){
-            int u, v, w;
-            scanf("%d%d%d", &u, &v, &w);
-            pii x = mp(u, v);
-            pii y = mp(v, u);
-            if (!C[x]) C[x] = w;
-            if (!C[y]) C[y] = w;
-            C[x] = min(C[x], w);
-            C[y] = min(C[y], w);
-            G.pb({u, v, w});
-            graph[u].pb(v);
-            graph[v].pb(u);
-        }
-        MST();
  
-        for (int i=1; i<=n; i++){
-            if (!vis[i]){
-                ++cnt;
-                res += airport;
-                dfs(i);
-            }
+        reset(n);
+ 
+        for (int i = 1; i <= m; i++){
+            int u, v, w; scanf("%d%d%d", &u, &v, &w);
+            if (w >= airport) continue;
+            G.push_back({u, v, w});
         }
  
-        printf("Case %d: %lld %d\n", cs, res, cnt);
+        int mstCost = mst();
+ 
+        set<int> comps; // number of components
+        for (int i = 1; i <= n; i++){
+            // insert parent of every node into the set
+            // to get number of components in the graph
+            comps.insert(Find(i));
+        }
+        int total = mstCost + comps.size() * airport;
+        printf("Case %d: %d %d\n", cs, total, comps.size());
     }
     return 0;
 }
